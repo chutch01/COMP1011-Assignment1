ab151ddd112a000dbb83c179bfd5ae28f5e91386
chutch01 <chutch01@hotmail.com>
2015-01-26 14:15:34 -0500

    created Hero class, added version and basic structure

---
+++ COMP1011-Assignment1/src/Hero.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ * @author chutc_000
+ * @program Description: Assignment to make a hero do some basic functions
+ * @version:0.1
+ * Assignment 1 Worth 10%
+ * due Jan 30th
+ */
+public class Hero {
+//private variables
+	
+	private int strength;
+	private int speed;
+	private int health;
+	
+//public variables	
+	public String name;
+
+//constructor
+public Hero(String name){
+	this.name = name;
+	generateAbilities();
+}
+
+//public methods
+public void fight() {
+
+}
+
+
+
+
+
+//private methods
+private void generateAbilities(){
+	
+}
+
+private void hitAttempt(){
+	
+}
+
+private void hitDamage(){
+	
+}
+
+
+
+
+
+
+
+
+}
